e50d8e7681b7b54790144926be8d894a2a02cc13
Darren Edale <darren@equituk.net>
2019-05-19 16:05:03 +0100

    Moved tr() convenience function from Application to i18n include  file. All free-standing functions in string and i18n includes in a global namespace declaration.

--- includes/string.php
+++ includes/string.php
@@ -6,121 +6,125 @@
  * Time: 20:23
  */
 
-/**
- * @param mixed $var Value/object to stringify
- *
- * @return string The string representation of the provided value or object.
- */
-function stringify($var): string {
-	if(is_string($var)) {
-		return $var;
-	}
+namespace {
 
-	if(is_numeric($var)) {
-		return "$var";
-	}
-
-	if(is_null($var)) {
-		return "NULL";
-	}
-
-	if(is_bool($var)) {
-		return ($var ? "TRUE" : "FALSE");
-	}
-
-	if(is_array($var)) {
-		$ret = [];
-
-		foreach($var as $k => $o) {
-			$ret[] = stringify($k) . "=>" . stringify($o);
+	/**
+	 * @param mixed $var Value/object to stringify
+	 *
+	 * @return string The string representation of the provided value or object.
+	 */
+	function stringify($var): string {
+		if (is_string($var)) {
+			return $var;
 		}
 
-		return "ARRAY[" . implode(",", $ret) . "]";
-	}
-
-	if(is_resource($var)) {
-		return "RESOURCE<" . get_resource_type($var) . ">";
-	}
-
-	if(is_object($var)) {
-		if($var instanceof DateTime) {
-			return $var->format("Y-m-d H:i:s");
+		if (is_numeric($var)) {
+			return "$var";
 		}
 
-		if(is_callable([$var, "__toString"], false)) {
-			/** @noinspection PhpUndefinedMethodInspection */
-			return $var->__toString();
+		if (is_null($var)) {
+			return "NULL";
 		}
 
-		return "OBJECT<" . get_class($var) . ">";
+		if (is_bool($var)) {
+			return ($var ? "TRUE" : "FALSE");
+		}
+
+		if (is_array($var)) {
+			$ret = [];
+
+			foreach ($var as $k => $o) {
+				$ret[] = stringify($k) . "=>" . stringify($o);
+			}
+
+			return "ARRAY[" . implode(",", $ret) . "]";
+		}
+
+		if (is_resource($var)) {
+			return "RESOURCE<" . get_resource_type($var) . ">";
+		}
+
+		if (is_object($var)) {
+			if ($var instanceof DateTime) {
+				return $var->format("Y-m-d H:i:s");
+			}
+
+			if (is_callable([$var, "__toString"], false)) {
+				/** @noinspection PhpUndefinedMethodInspection */
+				return $var->__toString();
+			}
+
+			return "OBJECT<" . get_class($var) . ">";
+		}
+
+		return "<unknown object>";
 	}
 
-	return "<unknown object>";
-}
-
-/**
- * Escape some content for inclusion in the page.
- *
- * Any characters in the string that have syntactic meaning in HTML are escaped such that they will be interpreted by
- * the user agent as a normal text character rather than something meaningful in HTML. The content provided must be
- * UTF-8 encoded, and the returned, escaped, content will be UTF-8 encoded too.
- *
- * @param $str string The content to escape.
- *
- * @return string The escaped content.
- */
-function html(string $str) {
-	return htmlentities($str, ENT_COMPAT, "UTF-8");
-}
-
-/**
- * Escape content for use as the data in a CSV cell.
- *
- * @param $content string The content to escape.
- *
- * @return string The escaped content.
- */
-function escapeCsvCell(string $content): string {
-	return str_replace("\"", "\\\"", $content);
-}
-
-/**
- * Build a string based on a template and data to insert into it.
- *
- * This method uses placeholders starting with a '\%' character, followed by an index number starting at 1 for the
- * first argument and incrementing by one for each subsequent argument up to the total number of arguments in the
- * string.
- *
- * For example, given the string "Hello %1, my name is %2" and the extra arguments "Darren" and "Susan", this method
- * will return the string "Hello Darren, my name is Susan".
- *
- * This method is used by the translation function tr() to automatically insert data into translated strings.
- *
- * @param $template string The template to process.
- * @param $args ...string The values to insert into the template.
- *
- * @return string The populated string.
- */
-function buildString(string $template, ... $args): string {
-	$argc = count($args);
-
-	if(0 == $argc) {
-		/* avoid pointlessly executing the rest ... */
-		return $template;
+	/**
+	 * Escape some content for inclusion in the page.
+	 *
+	 * Any characters in the string that have syntactic meaning in HTML are escaped such that they will be interpreted by
+	 * the user agent as a normal text character rather than something meaningful in HTML. The content provided must be
+	 * UTF-8 encoded, and the returned, escaped, content will be UTF-8 encoded too.
+	 *
+	 * @param $str string The content to escape.
+	 *
+	 * @return string The escaped content.
+	 */
+	function html(string $str) {
+		return htmlentities($str, ENT_COMPAT, "UTF-8");
 	}
 
-	/* set up the array of placeholders */
-	$placeholders = new SplFixedArray($argc);
-
-	for($i = $argc; $i > 0; ) {
-		/* NOTE the sprintf() is called before $placeholders[$i] is set, so
-		 * when it's set the post-decrement of $i has already been done. so
-		 * the index set in $placeholders is one less than the number
-		 * written into the placeholder string, which is what we want. this
-		 * slightly obscure way of doing it just saves one integer addition
-		 * or subtraction operation */
-		$placeholders[$i] = '%' . sprintf('%d', $i--);
+	/**
+	 * Escape content for use as the data in a CSV cell.
+	 *
+	 * @param $content string The content to escape.
+	 *
+	 * @return string The escaped content.
+	 */
+	function escapeCsvCell(string $content): string {
+		return str_replace("\"", "\\\"", $content);
 	}
 
-	return str_replace($placeholders->toArray(), $args, $template);
+	/**
+	 * Build a string based on a template and data to insert into it.
+	 *
+	 * This method uses placeholders starting with a '\%' character, followed by an index number starting at 1 for the
+	 * first argument and incrementing by one for each subsequent argument up to the total number of arguments in the
+	 * string.
+	 *
+	 * For example, given the string "Hello %1, my name is %2" and the extra arguments "Darren" and "Susan", this method
+	 * will return the string "Hello Darren, my name is Susan".
+	 *
+	 * This method is used by the translation function tr() to automatically insert data into translated strings.
+	 *
+	 * @param $template string The template to process.
+	 * @param $args ...string The values to insert into the template.
+	 *
+	 * @return string The populated string.
+	 */
+	function buildString(string $template, ... $args): string {
+		$argc = count($args);
+
+		if (0 == $argc) {
+			/* avoid pointlessly executing the rest ... */
+			return $template;
+		}
+
+		/* set up the array of placeholders */
+		$placeholders = new SplFixedArray($argc);
+
+		for ($i = $argc; $i > 0;) {
+			/* NOTE the sprintf() is called before $placeholders[$i] is set, so
+			 * when it's set the post-decrement of $i has already been done. so
+			 * the index set in $placeholders is one less than the number
+			 * written into the placeholder string, which is what we want. this
+			 * slightly obscure way of doing it just saves one integer addition
+			 * or subtraction operation */
+			$placeholders[$i] = '%' . sprintf('%d', $i--);
+		}
+
+		return str_replace($placeholders->toArray(), $args, $template);
+	}
+
 }
