4e2893978a4f5ca4cd878eb4c60bd7c393e64b11
Darren Edale <darren@equituk.net>
2019-05-26 21:35:07 +0100

    Added smoke test index file

---
+++ test/smoke/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+</head>
+<body>
+<ul>
+	<li><a href="listedit.php">ListEdit</a></li>
+	<?php
+	echo "<li>PHP</li>";
+	?>
+</ul>
+</body>
+</html>
