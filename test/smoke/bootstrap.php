8091a4dd98c510b66c20e012e8ee7cbe83d9886b
Darren Edale <darren@equituk.net>
2022-05-15 22:04:15 +0100

    Updated smoke test scripts
    - these are likely to be disposed of anyway

--- test/smoke/bootstrap.php
+++ test/smoke/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
-require_once "../../autoload.php";
+/**
+ * @author Darren Edale
+ * @version 1.2.0
+ */
+
+require_once __DIR__ . "/../../src/autoload.php";
 
 set_include_path(realpath(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "..") . PATH_SEPARATOR . get_include_path());
