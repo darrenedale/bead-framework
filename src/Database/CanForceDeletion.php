9581b84fdeb7d858d69c1d15cef5498c5e600ef3
Darren Edale <darren@equituk.net>
2022-06-05 21:53:27 +0100

    Updated CanForceDeletion trait
    - added abstract methods to trait to enforce constraints on the classes that can import it

--- src/Database/CanForceDeletion.php
+++ src/Database/CanForceDeletion.php
@@ -6,16 +6,36 @@ use PDO;
 
 /**
  * Endow a Model class with a forceDelete() method that forcibly deletes the model from the database.
- *
- * These properties are type-hinted for the benefit of the IDE. They are all provided by the Model class, which should
- * always be in the inheritance chain for classes that import this trait.
- *
- * @property PDO $connection
- * @property string $table
- * @property string $primaryKey
  */
 trait CanForceDeletion
 {
+    /**
+     * Fetch the connection to use when soft-deleting/restoring the model.
+     *
+     * This is a constraint to ensure the trait can only successfully be applied to Model (or Model-like) classes.
+     *
+     * @return PDO The connection to use when soft-deleting/restoring the model.
+     */
+    public abstract function connection(): PDO;
+
+    /**
+     * Fetch the table to use when soft-deleting/restoring the model.
+     *
+     * This is a constraint to ensure the trait can only successfully be applied to Model (or Model-like) classes.
+     *
+     * @return string The database table.
+     */
+    public abstract static function table(): string;
+
+    /**
+     * Fetch the primary key column to use when soft-deleting/restoring the model.
+     *
+     * This is a constraint to ensure the trait can only successfully be applied to Model (or Model-like) classes.
+     *
+     * @return string The primary key column.
+     */
+    public abstract static function primaryKey(): string;
+
     /**
      * Force deletion of the model.
      *
@@ -26,8 +46,8 @@ trait CanForceDeletion
      */
     public function forceDelete(): bool
     {
-        return $this->connection
-            ->prepare("DELETE FROM `" . static::$table . "` AS `t` WHERE `t`.`" . static::$primaryKey . "` = ? LIMIT 1")
-            ->execute($this->{static::$primaryKey});
+        return $this->connection()
+            ->prepare("DELETE FROM `" . static::table() . "` AS `t` WHERE `t`.`" . static::primaryKey() . "` = ? LIMIT 1")
+            ->execute($this->{static::primaryKey()});
     }
 }
