64d80672c5067945e5240894d7a10b6180432359
Darren Edale <darren@equituk.net>
2022-07-01 19:27:18 +0100

    Updated Json rule
    - fixed rule does not pass with empty array and likely some other "empty" values.

--- src/Validation/Rules/Json.php
+++ src/Validation/Rules/Json.php
@@ -28,11 +28,16 @@ class Json implements Rule
      */
     public function passes(string $field, $data): bool
     {
+		if (!is_string($data)) {
+			return false;
+		}
         try {
-            return is_string($data) && json_decode($data, false, 512, JSON_THROW_ON_ERROR);
+			json_decode($data, false, 512, JSON_THROW_ON_ERROR);
         } catch (Throwable $err) {
             return false;
         }
+
+		return true;
     }
 
     /**
