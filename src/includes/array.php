8241f7d45f182fee8ca9cc7c8bbc216a7d823263
Darren Edale <darrenedale@users.noreply.github.com>
2022-09-17 23:00:19 +0100

    Polyfill for array_is_list() (#24)

--- src/includes/array.php
+++ src/includes/array.php
@@ -134,3 +134,20 @@ if (!function_exists("removeEmptyElements")) {
         });
     }
 }
+
+if (!function_exists("array_is_list")) {
+    function array_is_list(array $arr): bool
+    {
+        $idx = 0;
+
+        foreach (array_keys($arr) as $key) {
+            if ($key !== $idx) {
+                return false;
+            }
+
+            ++$idx;
+        }
+
+        return true;
+    }
+}
