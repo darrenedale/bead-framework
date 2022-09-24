28159756d052c16e359aaa457644c3c5d3b31c03
Darren Edale <darrenedale@users.noreply.github.com>
2022-09-24 18:46:11 +0100

    Timestamp convenience classes (#26)
    * Added convenience classes timestamps
    
    - Hours, Days and Minutes classes that expressively convert to seconds
    
    * Added convenience classes timestamps
    
    - Hours, Days and Minutes classes that expressively convert to seconds
    
    * WIP
    
    * Added named type classes to deal with conversion of time periods to seconds for timestamp arithmetic.

---
+++ src/Util/Hours.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Equit\Util;
+
+/**
+ * An immutable number of hours to express as seconds.
+ */
+class Hours implements TimestampInterval
+{
+	/** @var int The number of seconds in an hour. */
+	public const SecondsPerHour = 60 * 60;
+
+	/** @var int The number of hours. */
+    private int $m_hours;
+
+	/**
+	 * Initialise a new number of hours to express as seconds.
+	 *
+	 * @param int $hours The number of hours.
+	 */
+    public function __construct(int $hours)
+	{
+        $this->m_hours = $hours;
+    }
+
+	/**
+	 * Fluently add hours.
+	 *
+	 * @param int $hours The number of hours to add.
+	 *
+	 * @return $this A clone of the Hours object with the given hours added.
+	 */
+	public function plus(int $hours): self
+	{
+		$clone = clone $this;
+		$clone->m_hours += $hours;
+		return $clone;
+	}
+
+	/**
+	 * Fluently subtract hours.
+	 *
+	 * @param int $hours The number of hours to subtract.
+	 *
+	 * @return $this A clone of the Hours object with the given hours subtracted.
+	 */
+	public function minus(int $hours): self
+	{
+		$clone = clone $this;
+		$clone->m_hours -= $hours;
+		return $clone;
+	}
+
+	/**
+	 * Fetch the number of hours.
+	 *
+	 * @return int The number of hours.
+	 */
+	public function hours(): int
+	{
+		return $this->m_hours;
+	}
+
+	/**
+	 * Fetch the number of seconds in the number of hours.
+	 *
+	 * @return int The number of seconds.
+	 */
+    public function inSeconds(): int
+	{
+        return $this->m_hours * self::SecondsPerHour;
+    }
+}
---
+++ test/Util/HoursTest.php
@@ -0,0 +1,238 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Equit\Test\Util;
+
+use Equit\Test\Framework\TestCase;
+use Equit\Util\Hours;
+use TypeError;
+
+class HoursTest extends TestCase
+{
+	/**
+	 * Test data for testConstructor
+	 *
+	 *@return iterable The test data.
+	 */
+	public function dataForTestConstructor(): iterable
+	{
+		for ($hours = -72; $hours <= 72; ++$hours) {
+			yield "typical{$hours}Hours" => [$hours,];
+		}
+
+		yield from [
+			"extremeIntMax" => [PHP_INT_MAX,],
+			"extremeIntMin" => [PHP_INT_MIN,],
+			"invalidFloat" => [3.1415927, TypeError::class,],
+			"invalidBool" => [true, TypeError::class,],
+			"invalidNull" => [null, TypeError::class,],
+			"invalidString" => ["42", TypeError::class,],
+			"invalidArray" => [[42,], TypeError::class,],
+			"invalidObject" => [
+				new class
+				{
+					public function hours(): int
+					{
+						return 42;
+					}
+				},
+				TypeError::class,
+			],
+			"invalidClosure" => [fn(): int => 42, TypeError::class,],
+		];
+	}
+
+	/**
+	 * Ensure the constructor throws with invalid data.
+	 *
+	 * @dataProvider dataForTestConstructor
+	 *
+	 * @param mixed $hours The hours to test with.
+	 * @param string|null $exceptionCalss The exception expected, if any.
+	 */
+	public function testConstructor($hours, ?string $exceptionCalss = null): void
+	{
+		if (isset($exceptionCalss)) {
+			$this->expectException($exceptionCalss);
+		}
+
+		$testObject = new Hours($hours);
+		$this->assertEquals($hours, $testObject->hours());
+	}
+
+	/**
+	 * Test data for testConstructor
+	 *
+	 *@return iterable The test data.
+	 */
+	public function dataForTestHours(): iterable
+	{
+		for ($hours = -72; $hours <= 72; ++$hours) {
+			yield "typical{$hours}Hours" => [$hours,];
+		}
+
+		yield from [
+			"extremeIntMax" => [PHP_INT_MAX,],
+			"extremeIntMin" => [PHP_INT_MIN,],
+		];
+	}
+
+	/**
+	 * @dataProvider dataForTestHours
+	 *
+	 * @param int $hours The number of hours to test with.
+	 */
+	public function testHours(int $hours): void
+	{
+		$testObject = new Hours($hours);
+		$this->assertEquals($hours, $testObject->hours());
+	}
+
+	/**
+	 * Test data for testPlus().
+	 *
+	 * @return iterable The test data.
+	 */
+	public function dataForTestPlus(): iterable
+	{
+		yield from [
+			"typical0Positive" => [0, 42, 42,],
+			"typical0Negative" => [0, -42, -42,],
+			"typicalNegative0" => [-10, 10, 0,],
+			"typicalPositive0" => [10, -10, 0,],
+			"typicalNegativePositivePositive" => [-38, 51, 13,],
+			"typicalNegativePositiveNegative" => [-38, 21, -17,],
+			"typicalNegativeNegativeNegative" => [-38, -12, -50,],
+			"typicalPositivePositivePositive" => [12, 14, 26,],
+			"typicalPositiveNegativePositive" => [42, -21, 21,],
+			"typicalPositiveNegativeNegative" => [15, -31, -16,],
+
+			"invalidFloat" => [42, 3.1415927, 0, TypeError::class,],
+			"invalidBool" => [42, true, 0, TypeError::class,],
+			"invalidNull" => [42, null, 0, TypeError::class,],
+			"invalidString" => [42, "42", 0, TypeError::class,],
+			"invalidArray" => [42, [42,], 0, TypeError::class,],
+			"invalidObject" => [
+				42,
+				new class
+				{
+					public function hours(): int
+					{
+						return 42;
+					}
+				},
+				0,
+				TypeError::class,
+			],
+			"invalidClosure" => [42, fn(): int => 42, 0, TypeError::class,],
+		];
+	}
+
+	/**
+	 * Ensure plus() performs the correct calculation and preserves immutability.
+	 *
+	 * @dataProvider dataForTestPlus
+	 *
+	 * @param int $initial The number of hours in the test object.
+	 * @param mixed $add The value to add.
+	 * @param int $expected The expected number of hours after the addition.
+	 * @param string|null $exceptionClass The exception expected, if any.
+	 */
+	public function testPlus(int $initial, $add, int $expected, ?string $exceptionClass = null): void
+	{
+		if (isset($exceptionClass)) {
+			$this->expectException($exceptionClass);
+		}
+
+		$hours = new Hours($initial);
+		$actual = $hours->plus($add);
+		$this->assertNotSame($hours, $actual);
+		$this->assertEquals($initial, $hours->hours());
+		$this->assertEquals($expected, $actual->hours());
+	}
+
+	/**
+	 * Test data for testPlus().
+	 *
+	 * @return iterable The test data.
+	 */
+	public function dataForTestMinus(): iterable
+	{
+		yield from [
+			"typical0Negative" => [0, 42, -42,],
+			"typical0Positive" => [0, -42, 42,],
+			"typicalNegative0" => [-10, -10, 0,],
+			"typicalPositive0" => [10, 10, 0,],
+			"typicalNegativeNegativePositive" => [-38, -51, 13,],
+			"typicalNegativePositiveNegative" => [-38, 12, -50,],
+			"typicalNegativeNegativeNegative" => [-38, -12, -26,],
+			"typicalPositivePositivePositive" => [42, 14, 28,],
+			"typicalPositiveNegativePositive" => [42, -21, 63,],
+			"typicalPositivePositiveNegative" => [15, 31, -16,],
+
+			"invalidFloat" => [42, 3.1415927, 0, TypeError::class,],
+			"invalidBool" => [42, true, 0, TypeError::class,],
+			"invalidNull" => [42, null, 0, TypeError::class,],
+			"invalidString" => [42, "42", 0, TypeError::class,],
+			"invalidArray" => [42, [42,], 0, TypeError::class,],
+			"invalidObject" => [
+				42,
+				new class
+				{
+					public function hours(): int
+					{
+						return 42;
+					}
+				},
+				0,
+				TypeError::class,
+			],
+			"invalidClosure" => [42, fn(): int => 42, 0, TypeError::class,],
+		];
+	}
+
+	/**
+	 * Ensure minus() performs the correct calculation and preserves immutability.
+	 *
+	 * @dataProvider dataForTestMinus
+	 *
+	 * @param int $initial The number of hours in the test object.
+	 * @param mixed $sub The value to subtract.
+	 * @param int $expected The expected number of hours after the addition.
+	 * @param string|null $exceptionClass The exception expected, if any.
+	 */
+	public function testMinus(int $initial, $sub, int $expected, ?string $exceptionClass = null): void
+	{
+		if (isset($exceptionClass)) {
+			$this->expectException($exceptionClass);
+		}
+
+		$hours = new Hours($initial);
+		$actual = $hours->minus($sub);
+		$this->assertNotSame($hours, $actual);
+		$this->assertEquals($initial, $hours->hours());
+		$this->assertEquals($expected, $actual->hours());
+	}
+
+	/**
+	 * Test data for testInSeconds()
+	 *
+	 * @return iterable The test data.
+	 */
+	public function dataForTestInSeconds(): iterable
+	{
+		for ($hours = -72; $hours <= 72; ++$hours)
+			yield "typical{$hours}Hours" => [$hours, Hours::SecondsPerHour * $hours,];
+
+	}
+
+	/**
+	 * Ensure the conversion behaves as expected.
+	 * @dataProvider dataForTestInSeconds
+	 */
+	public function testInSeconds(int $hours, int $expectedSeconds): void
+	{
+		$this->assertEquals($expectedSeconds, (new Hours($hours))->inSeconds());
+	}
+}
