204093f0da60b0dcfc9990dd5695687d3ed0ecf7
Darren Edale <darrenedale@users.noreply.github.com>
2022-10-23 00:59:30 +0100

    BEAD-30 application as psr 11 container (#30)
    * Implement service container interface in Application base class
    
    - ServiceContainer contract
    - Application implements contract and PSR11 ContainerInterface
    - Added PSR dependency to composer
    - Added PHP version dependency to composer
    - Translator contract
    - Translator implements contract
    - Translator bound to service in Application
    - Exceptions supporting PSR11 container
    - unit test for new Application methods
    
    * Fix unnecessary import of invalid class
    
    - typo ContainerIn
    
    * Fix incorrect signature of method in Translator contract
    
    - added unit test for Translator
    - updated unit test for Application to include tear down of test fixture
    
    * Fix incorrect signature of method in Translator contract
    
    - added unit test for Translator
    - updated unit test for Application to include tear down of test fixture
    
    Co-authored-by: Darren Edale <darren.edale@equit.dev>

--- src/Contracts/Translator.php
+++ src/Contracts/Translator.php
@@ -5,7 +5,7 @@ namespace Equit\Contracts;
 interface Translator
 {
     public function language(): string;
-    public function setLanguage(): string;
+    public function setLanguage(string $language): void;
     public function hasTranslation(string $string): bool;
     public function translate(string $string): string;
 }
