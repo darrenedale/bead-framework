2e98ed914112b7e7ff4cc05e092e8aa31f6afb43
Darren Edale <darren@equituk.net>
2022-11-06 18:52:18 +0000

    Updated UriSigner
    - parameters are now passed to sign() instead of stored with the instance.
    - expiry is a required argument for sign() - all signed URIs must have an expiry

--- src/Contracts/UriSigner.php
+++ src/Contracts/UriSigner.php
@@ -19,17 +19,6 @@ interface UriSigner
 	public function usingSecret(string $secret): self;
 
 	/**
-	 * Fluently set the parameters to use when signing URIs.
-	 *
-	 * UriSigners are immutable, a clone of the current signer is altered and returned.
-	 *
-	 * @param array $parameters The paramters.
-	 *
-	 * @return $this A UriSigner for further methdo chaining.
-	 */
-	public function withParameters(array $parameters): self;
-
-	/**
 	 * Fetch the secret to use to sign URIs.
 	 *
 	 * @return string The secret.
@@ -37,20 +26,18 @@ interface UriSigner
 	public function secret(): string;
 
 	/**
-	 * Fetch the URI parameters to use in the signing process.
+	 * Sign the given URI with the configured secret.
 	 *
-	 * @return array The parameters.
-	 */
-	public function parameters(): array;
-
-	/**
-	 * Sign the given URI with the configured secret and parameters.
+	 * The provided parameters, if not empty, will be appended to the URI. How the signing and expiry are handled is
+	 * implementation-defined.
 	 *
 	 * @param string $uri The URI to sign.
+	 * @param array<string,string> $parameters The parameters for the URI.
+	 * @param int|DateTimeInterface $expires The point in time at which the signed URI expires = null.
 	 *
 	 * @return string The signed URI.
 	 */
-	public function sign(string $uri): string;
+	public function sign(string $uri, array $parameters, $expires): string;
 
 	/**
 	 * Verify a signed URI.
