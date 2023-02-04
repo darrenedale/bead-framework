2d00109fb83d7510533306b1617e1fd38c509b63
Darren Edale <darrenedale@users.noreply.github.com>
2023-02-04 22:30:31 +0000

    BEAD-99 Updated StaticXRayException (#72)
    * Updated StaticXRayException
    
    - added unit test
    - fix SessionFileSaveExceptionTest

--- test/Exceptions/Testing/StaticXRayExceptionTest.php
+++ test/Exceptions/Testing/StaticXRayExceptionTest.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace BeadTests\Exceptions\Session;
+namespace BeadTests\Exceptions\Testing;
 
-use Bead\Exceptions\Session\SessionFileSaveException;
+use Bead\Exceptions\Testing\StaticXRayException;
 use BeadTests\Exceptions\AssertsCommonExceptionProperties;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 
-final class SessionFileSaveExceptionTest extends TestCase
+final class StaticXRayExceptionTest extends TestCase
 {
     use AssertsCommonExceptionProperties;
 
     /** @var string The session ID to test with. */
-    private const TesFilename = "/tmp/bead-framework/sessions/session-file";
+    private const TesClassName = "Bead\\TestClass";
 
-    /** Ensure the ID of the missing session can be set in the exception constructor. */
+    /** Ensure the class name of the xrayed class can be set in the exception constructor. */
     public function testConstructor(): void
     {
-        $exception = new SessionFileSaveException(self::TesFilename);
-        self::assertEquals(self::TesFilename, $exception->getFileName());
+        $exception = new StaticXRayException(self::TesClassName);
+        self::assertEquals(self::TesClassName, $exception->getClassName());
         self::assertCode(0, $exception);
         self::assertMessage("", $exception);
         self::assertPrevious(null, $exception);
@@ -27,8 +27,8 @@ final class SessionFileSaveExceptionTest extends TestCase
     /** Ensure we can set an exception code in the constructor. */
     public function testConstructorWithCode(): void
     {
-        $exception = new SessionFileSaveException(self::TesFilename, code: 42);
-        self::assertEquals(self::TesFilename, $exception->getFileName());
+        $exception = new StaticXRayException(self::TesClassName, code: 42);
+        self::assertEquals(self::TesClassName, $exception->getClassName());
         self::assertCode(42, $exception);
         self::assertMessage("", $exception);
         self::assertPrevious(null, $exception);
@@ -37,8 +37,8 @@ final class SessionFileSaveExceptionTest extends TestCase
     /** Ensure we can set an message in the constructor. */
     public function testConstructorWithMessage(): void
     {
-        $exception = new SessionFileSaveException(self::TesFilename, message: "The meaning of life.");
-        self::assertEquals(self::TesFilename, $exception->getFileName());
+        $exception = new StaticXRayException(self::TesClassName, message: "The meaning of life.");
+        self::assertEquals(self::TesClassName, $exception->getClassName());
         self::assertCode(0, $exception);
         self::assertMessage("The meaning of life.", $exception);
         self::assertPrevious(null, $exception);
@@ -48,8 +48,8 @@ final class SessionFileSaveExceptionTest extends TestCase
     public function testConstructorWithPrevious(): void
     {
         $previous = new RuntimeException();
-        $exception = new SessionFileSaveException(self::TesFilename, previous: $previous);
-        self::assertEquals(self::TesFilename, $exception->getFileName());
+        $exception = new StaticXRayException(self::TesClassName, previous: $previous);
+        self::assertEquals(self::TesClassName, $exception->getClassName());
         self::assertCode(0, $exception);
         self::assertMessage("", $exception);
         self::assertPrevious($previous, $exception);
