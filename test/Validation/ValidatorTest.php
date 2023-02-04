6b7c9b6b389853539a49dac7f4f73472825588d0
Darren Edale <darren@equituk.net>
2023-02-04 12:33:57 +0000

    Skeleton class for Validator unit test

---
+++ test/Validation/ValidatorTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Validation;
+
+use BeadTests\Framework\TestCase;
+
+final class ValidatorTest extends TestCase
+{
+	public function testSkipRemainingRules(): void 
+    {
+    }
+
+	public function testFails(): void 
+    {
+    }
+
+	public function testFieldsUnderValidation(): void 
+    {
+    }
+
+	public function testErrors(): void 
+    {
+    }
+
+	public function test__construct(): void 
+    {
+    }
+
+	public function testSetData(): void 
+    {
+    }
+
+	public function testRules(): void 
+    {
+    }
+
+	public function testValidated(): void 
+    {
+    }
+
+	public function testClearErrors(): void 
+    {
+    }
+
+	public function testValidate(): void 
+    {
+    }
+
+	public function testRegisterRuleAlias(): void 
+    {
+    }
+
+	public function testData(): void 
+    {
+    }
+
+	public function testPasses(): void 
+    {
+    }
+
+	public function testAddRule(): void 
+    {
+    }
+
+	public function testIsValidating(): void
+    {
+    }
+}
