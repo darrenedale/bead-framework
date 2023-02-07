caa4676814c02ea2f7b629cfefaa6ec018c7beaf
Darren Edale <darren.edale@equit.dev>
2023-02-07 19:29:23 +0000

    Updated DuplicateRouteParameterNameException
    - added unit test

---
+++ test/Exceptions/DuplicateRouteParameterNameExceptionTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeadTests\Exceptions;
+
+use Bead\Exceptions\DuplicateRouteParameterNameException;
+use BeadTests\Framework\TestCase;
+use Mockery;
+use RuntimeException;
+
+final class DuplicateRouteParameterNameExceptionTest extends TestCase
+{
+	use AssertsCommonExceptionProperties;
+
+	/** Ensure the erroneous request can be set in the exception constructor. */
+	public function testConstructor(): void
+	{
+		$exception = new DuplicateRouteParameterNameException("invalid-parameter", "/home/route");
+		self::assertEquals("invalid-parameter", $exception->getParameterName());
+		self::assertEquals("/home/route", $exception->getRoute());
+		self::assertCode(0, $exception);
+		self::assertMessage("", $exception);
+		self::assertPrevious(null, $exception);
+	}
+
+	/** Ensure we can set an exception code in the constructor. */
+	public function testConstructorWithCode(): void
+	{
+        $exception = new DuplicateRouteParameterNameException("invalid-parameter", "/home/route", code: 42);
+        self::assertEquals("invalid-parameter", $exception->getParameterName());
+        self::assertEquals("/home/route", $exception->getRoute());
+        self::assertCode(42, $exception);
+        self::assertMessage("", $exception);
+        self::assertPrevious(null, $exception);
+	}
+
+	/** Ensure we can set a message in the constructor. */
+	public function testConstructorWithMessage(): void
+	{
+        $exception = new DuplicateRouteParameterNameException("invalid-parameter", "/home/route", message: "The meaning of life.");
+        self::assertEquals("invalid-parameter", $exception->getParameterName());
+        self::assertEquals("/home/route", $exception->getRoute());
+        self::assertCode(0, $exception);
+		self::assertMessage("The meaning of life.", $exception);
+		self::assertPrevious(null, $exception);
+	}
+
+	/** Ensure we can set a previous exception in the constructor. */
+	public function testConstructorWithPrevious(): void
+	{
+        $previous = new RuntimeException();
+        $exception = new DuplicateRouteParameterNameException("invalid-parameter", "/home/route", previous: $previous);
+        self::assertEquals("invalid-parameter", $exception->getParameterName());
+        self::assertEquals("/home/route", $exception->getRoute());
+		self::assertCode(0, $exception);
+		self::assertMessage("", $exception);
+		self::assertPrevious($previous, $exception);
+	}
+}
