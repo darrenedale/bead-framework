7d391c78cd1be0ef73cb38da3028ba33515c2bbb
Darren Edale <darrenedale@users.noreply.github.com>
2023-12-03 14:06:21 +0000

    BEAD-174 check exception constructors (#133)
    * Remove redundant constructor.
    * Remove unused imports.

--- src/Exceptions/Http/CsrfTokenVerificationException.php
+++ src/Exceptions/Http/CsrfTokenVerificationException.php
@@ -2,27 +2,9 @@
 
 namespace Bead\Exceptions\Http;
 
-use Bead\Web\Request;
-use Throwable;
-
-/**
- * Exception thrown when a request fails CSRF validation.
- */
+/** Exception thrown when a request fails CSRF verification. */
 class CsrfTokenVerificationException extends HttpException
 {
-    /**
-     * Initialise a new instance of the exception.
-     *
-     * @param Request $request The request that failed CSRF validation.
-     * @param string $message The optional error message. Defaults to an empty string.
-     * @param int $code The optional error code. Defaults to 0.
-     * @param Throwable|null $previous The optional Throwable that was previously thrown. Defaults to null.
-     */
-    public function __construct(Request $request, string $message = "", int $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($request, $message, $code, $previous);
-    }
-
     /**
      * The HTTP status code.
      *
