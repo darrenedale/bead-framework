7496b6e3e8957ce7e7e0783f9580ab01f590eeb0
Darren Edale <darren@equituk.net>
2023-12-16 16:07:37 +0000

    Removed smoke test app from repository (it has its own repository).

--- test/smoke/views/components/inline-dialogue.php
+++
@@ -1,8 +0,0 @@
-<div class="dialogue inline-dialogue inline <?= $classes ?? "" ?>" id="<?= $id ?? "" ?>">
-    <?php if (is_string($title ?? null)): ?>
-        <div class="title"><?= html($title) ?></div>
-    <?php endif; ?>
-    <div class="content">
-        <?= $slot ?>
-    </div>
-</div>
