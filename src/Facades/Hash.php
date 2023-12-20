1a9e72734ca67e9f358d9bd74b487bc3c0655b18
Darren Edale <darrenedale@users.noreply.github.com>
2023-12-20 22:58:11 +0000

    BEAD-189 hasher (#149)

---
+++ src/Facades/Hash.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bead\Facades;
+
+use Bead\Contracts\Hasher;
+use Bead\Facades\ApplicationServiceFacade;
+
+/**
+ * @method string hash(string $value)
+ * @method string verify(string $value, string $hash)
+ */
+class Hash extends ApplicationServiceFacade
+{
+    protected static string $serviceInterface = Hasher::class;
+}
---
+++ test/Facades/HashTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeadTests\Facades;
+
+use Bead\Contracts\Hasher as HasherContract;
+use Bead\Core\Application;
+use Bead\Facades\Hash;
+use BeadTests\Framework\TestCase;
+use Mockery;
+use Mockery\MockInterface;
+
+final class HashTest extends TestCase
+{
+    /** @var Application&MockInterface The test Application instance. */
+    private Application $app;
+
+    /** @var HasherContract&MockInterface The test hasher instance bound into the application. */
+    private HasherContract $hasher;
+
+    public function setUp(): void
+    {
+        $this->hasher = Mockery::mock(HasherContract::class);
+        $this->app = Mockery::mock(Application::class);
+        $this->mockMethod(Application::class, "instance", $this->app);
+
+        $this->app->shouldReceive("get")
+            ->with(HasherContract::class)
+            ->andReturn($this->hasher)
+            ->byDefault();
+    }
+
+    public function tearDonw(): void
+    {
+        unset($this->app, $this->hasher);
+        Mockery::close();
+        parent::tearDown();
+    }
+
+    /** Ensure the hash the instance returns is returned by hash(). */
+    public function testHash1(): void
+    {
+        $this->hasher->shouldReceive("hash")
+            ->once()
+            ->with("user-provided-value")
+            ->andReturn("hashed-user-provided-value");
+
+        self::assertEquals("hashed-user-provided-value", Hash::hash("user-provided-value"));
+    }
+
+    /** Ensure false is returned from verify() when the instance returns true. */
+    public function testVerify1(): void
+    {
+        $this->hasher->shouldReceive("verify")
+            ->once()
+            ->with("user-provided-value", "stored-hashed-value")
+            ->andReturn(true);
+
+        self::assertTrue(Hash::verify("user-provided-value", "stored-hashed-value"));
+    }
+
+    /** Ensure false is returned from verify() when the instance returns false. */
+    public function testVerify2(): void
+    {
+        $this->hasher->shouldReceive("verify")
+            ->once()
+            ->with("user-provided-value", "stored-hashed-value")
+            ->andReturn(false);
+
+        self::assertFalse(Hash::verify("user-provided-value", "stored-hashed-value"));
+    }
+}
