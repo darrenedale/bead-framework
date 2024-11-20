4d6c591d5136436a05c587141361cbc1817805a4
Darren Edale <darren.edale@equit.dev>
2024-11-20 22:22:55 +0000

    Add message for exception when request can't be routed. Add unit test for handleRequest().

--- test/Web/ApplicationTest.php
+++ test/Web/ApplicationTest.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace BeadTests\Web;
 
+use Bead\Contracts\Web\RequestPostprocessor;
+use Bead\Contracts\Web\RequestPreprocessor;
+use Bead\Contracts\Web\Response as ResponseContract;
+use Bead\Contracts\Web\Router as RouterContract;
+use Bead\Exceptions\Http\NotFoundException;
+use Bead\Exceptions\UnroutableRequestException;
 use Bead\Facades\Session;
 use Bead\Testing\StaticXRay;
 use Bead\Testing\XRay;
 use Bead\Core\Application as CoreApplication;
 use Bead\Web\Application as WebApplication;
+use Bead\Web\Responses\AbstractResponse;
+use Bead\Web\Request;
 use BeadTests\Framework\TestCase;
+use Mockery;
 
 class ApplicationTest extends TestCase
 {
@@ -67,5 +76,170 @@ class ApplicationTest extends TestCase
         $app = new XRay($this->makeTestWebApplication());
         $app->loadPlugins();
         self::assertFalse($loadPluginCalled);
+    }
+
+    /** Ensure handleRequest() emits the expected events. */
+    public function testHandleRequest1(): void
+    {
+        $expectedEvents = [
+            "application.handlerequest.requestreceived",
+            "application.handlerequest.preprocessing",
+            "application.handlerequest.preprocessed",
+            "application.handlerequest.routing",
+            "application.handlerequest.routed",
+            "application.handlerequest.postprocessing",
+            "application.handlerequest.postprocessed",
+        ];
+
+        $actualEvents = [];
+        $app = self::makeTestWebApplication();
+        $router = Mockery::mock(RouterContract::class);
+        $expectedRequest = Mockery::mock(Request::class);
+        $expectedResponse = Mockery::mock(ResponseContract::class);
+        $router->expects("route")->with($expectedRequest)->andReturn($expectedResponse);
+        $app->setRouter($router);
+
+        $handler = static function(string $event, Request $request) use (&$actualEvents, $expectedRequest): void {
+            ApplicationTest::assertSame($expectedRequest, $request);
+            $actualEvents[] = $event;
+        };
+
+        foreach ($expectedEvents as $event) {
+            $app->connect($event, $handler);
+        }
+
+        $app->handleRequest($expectedRequest);
+        self::assertSame($expectedEvents, $actualEvents);
+    }
+
+    /** Ensure handleRequest() routes the request and returns the response from the router. */
+    public function testHandleRequest2(): void
+    {
+        $app = self::makeTestWebApplication();
+        $router = Mockery::mock(RouterContract::class);
+        $expectedRequest = Mockery::mock(Request::class);
+        $expectedResponse = Mockery::mock(ResponseContract::class);
+        $router->expects("route")->with($expectedRequest)->andReturn($expectedResponse);
+        $app->setRouter($router);
+
+        $actualReponse = $app->handleRequest($expectedRequest);
+        self::assertSame($expectedResponse, $actualReponse);
+    }
+
+    /** Ensure handleRequest() returns the response from a pre-processor. */
+    public function testHandleRequest3(): void
+    {
+        $app = self::makeTestWebApplication();
+        $router = Mockery::mock(RouterContract::class);
+        $expectedRequest = Mockery::mock(Request::class);
+        $expectedResponse = Mockery::mock(ResponseContract::class);
+        $postProcessor = Mockery::mock(RequestPreprocessor::class);
+        $router->shouldNotReceive("route");
+        $app->setRouter($router);
+
+        $postProcessor->expects("preprocessRequest")
+            ->with($expectedRequest)
+            ->andReturn($expectedResponse);
+
+        $app = new XRay($app);
+        $app->m_requestProcessors = [$postProcessor];
+
+        $actualReponse = $app->handleRequest($expectedRequest);
+        self::assertSame($expectedResponse, $actualReponse);
+    }
+
+    /** Ensure handleRequest() returns the response from the router if a pre-processor doesn't return a response. */
+    public function testHandleRequest4(): void
+    {
+        $app = self::makeTestWebApplication();
+        $router = Mockery::mock(RouterContract::class);
+        $expectedRequest = Mockery::mock(Request::class);
+        $expectedResponse = Mockery::mock(ResponseContract::class);
+        $preProcessor = Mockery::mock(RequestPreprocessor::class);
+        $app->setRouter($router);
+
+        $preProcessor->expects("preprocessRequest")
+            ->with($expectedRequest)
+            ->andReturn(null);
+
+        $router->expects("route")
+            ->with($expectedRequest)
+            ->andReturn($expectedResponse);
+
+        $app = new XRay($app);
+        $app->m_requestProcessors = [$preProcessor];
+
+        $actualReponse = $app->handleRequest($expectedRequest);
+        self::assertSame($expectedResponse, $actualReponse);
+    }
+
+    /** Ensure handleRequest() returns the response from a post-processor. */
+    public function testHandleRequest5(): void
+    {
+        $app = self::makeTestWebApplication();
+        $router = Mockery::mock(RouterContract::class);
+        $expectedRequest = Mockery::mock(Request::class);
+        $routedResponse = Mockery::mock(ResponseContract::class);
+        $postProcessedResponse = Mockery::mock(ResponseContract::class);
+        $postProcessor = Mockery::mock(RequestPostprocessor::class);
+        $app->setRouter($router);
+
+        $router->expects("route")
+            ->with($expectedRequest)
+            ->andReturn($routedResponse);
+
+        $postProcessor->expects("postProcessRequest")
+            ->with($expectedRequest, $routedResponse)
+            ->andReturn($postProcessedResponse);
+
+        $app = new XRay($app);
+        $app->m_requestProcessors = [$postProcessor];
+
+        $actualReponse = $app->handleRequest($expectedRequest);
+        self::assertSame($postProcessedResponse, $actualReponse);
+    }
+
+    /** Ensure handleRequest() returns the response from the router if a post-processor doesn't return a response. */
+    public function testHandleRequest6(): void
+    {
+        $app = self::makeTestWebApplication();
+        $router = Mockery::mock(RouterContract::class);
+        $expectedRequest = Mockery::mock(Request::class);
+        $expectedResponse = Mockery::mock(ResponseContract::class);
+        $postProcessor = Mockery::mock(RequestPostprocessor::class);
+        $app->setRouter($router);
+
+        $router->expects("route")
+            ->with($expectedRequest)
+            ->andReturn($expectedResponse);
+
+        $postProcessor->expects("postprocessRequest")
+            ->with($expectedRequest, $expectedResponse)
+            ->andReturn(null);
+
+        $app = new XRay($app);
+        $app->m_requestProcessors = [$postProcessor];
+
+        $actualReponse = $app->handleRequest($expectedRequest);
+        self::assertSame($expectedResponse, $actualReponse);
+    }
+
+    /** Ensure handleRequest() throws the expected NotFoundException when the router throws. */
+    public function testHandleRequest7(): void
+    {
+        $app = self::makeTestWebApplication();
+        $router = Mockery::mock(RouterContract::class);
+        $expectedRequest = Mockery::mock(Request::class);
+        $app->setRouter($router);
+
+        $router->expects("route")
+            ->with($expectedRequest)
+            ->andThrow(new UnroutableRequestException($expectedRequest));
+
+        $app = new XRay($app);
+
+        self::expectException(NotFoundException::class);
+        self::expectExceptionMessage("The requested page could not be found");
+        $actualReponse = $app->handleRequest($expectedRequest);
     }
 }
