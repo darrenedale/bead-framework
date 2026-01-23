eb1c2962283d6a5d9ec1333055cd78827aebb6ea
Darren Edale <darren@equituk.net>
2026-01-23 18:07:06 +0000

    Fix DownloadResponse does not send content-type header.

--- src/Web/Responses/DownloadResponse.php
+++ src/Web/Responses/DownloadResponse.php
@@ -15,7 +15,7 @@ class DownloadResponse extends AbstractResponse
     private string $m_fileName = "";
 
     /** @var string The content for the download. */
-    private string $m_data = "";
+    private string $m_data;
 
     /** @var HeaderContract[] The headers. */
     private array $m_headers = [];
@@ -117,8 +117,15 @@ class DownloadResponse extends AbstractResponse
             $fileName = "download";
         }
 
-        /** @psalm-suppress MissingThrowsDocblock - "content-disposition" is known to be a valid header name. */
-        return [...$this->m_headers, new Header("content-disposition", "attachment; filename=\"{$fileName}\"")];
+        /**
+         * @psalm-suppress MissingThrowsDocblock - "content-disposition" and "content-type" are known to be valid header
+         * names.
+         */
+        return [
+            ...$this->m_headers,
+            new Header("content-disposition", "attachment; filename=\"{$fileName}\""),
+            new Header("content-type", $this->contentType()),
+        ];
     }
 
     /**
