d5e6a604bca40aed6456240c23fdc34fab7030b5
Darren Edale <darren@equituk.net>
2026-02-21 12:42:39 +0000

    Update CS and SA helper scripts to enable specific PHP version to be used.

--- test/coding-standards.sh
+++ test/coding-standards.sh
@@ -1,14 +1,26 @@
 #! /usr/bin/env bash
 
 ROOT_PATH=$(realpath "$(dirname "$0")")
+PHP_VERSION=8.1
+CS_CONFIG=src
 
-# specify one of src, test, standards on the command-line
-if [ "" != "$1" ]; then
-  CS_CONFIG="$1"
-else
-  CS_CONFIG=src
-fi
+while [ "" != "$1" ]; do
+  case "$1" in
+    "-v")
+      shift
+      PHP_VERSION="$1"
+      ;;
+
+    *)
+      # specify one of src, test, standards on the command-line
+      CS_CONFIG="$1"
+      break 2
+      ;;
+  esac
+
+  shift
+done
 
 cd "$ROOT_PATH"
-docker compose up -d php83
-docker exec -it bead-test-83 ./vendor/bin/phpcs "$CS_CONFIG/"
+docker compose -f ../docker-compose.yml up -d "bead-${PHP_VERSION}"
+docker exec -it "bead-${PHP_VERSION}" ./vendor/bin/phpcs "${CS_CONFIG}/"
