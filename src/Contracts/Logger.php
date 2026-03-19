9f199e1819e8e422b4920c3d54bbdf1edd4e7906
Darren Edale <darren@equituk.net>
2026-03-19 19:34:43 +0000

    Tidy up constants in Logger contract.

--- src/Contracts/Logger.php
+++ src/Contracts/Logger.php
@@ -15,17 +15,17 @@ interface Logger extends LoggerInterface
 
     public const AlertLevel = 1;
 
-    public const CriticalLevel  = 2;
+    public const CriticalLevel = 2;
 
-    public const ErrorLevel     = 3;
+    public const ErrorLevel = 3;
 
-    public const WarningLevel   = 4;
+    public const WarningLevel = 4;
 
-    public const NoticeLevel    = 5;
+    public const NoticeLevel = 5;
 
-    public const InformationLevel      = 6;
+    public const InformationLevel = 6;
 
-    public const DebugLevel     = 7;
+    public const DebugLevel = 7;
 
     /**
      * Fetch the current logging level.
